23f729baf32d953177e7c03f55437fd3ed6e9f20
Nipunishara <nipunishara.info@gmail.com>
2021-02-28 17:46:39 +0530

    first commit

---
+++ ios_assignment_one/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_assignment_one
+//
+//  Created by Macbook on 2/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
