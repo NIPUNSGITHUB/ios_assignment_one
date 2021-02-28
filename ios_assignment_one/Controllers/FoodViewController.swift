31dde0f6e701d47638e41f0b956d97b3cf528935
Nipunishara <nipunishara.info@gmail.com>
2021-02-28 19:12:06 +0530

    tableview

--- ios_assignment_one/Controllers/FoodViewController.swift
+++ ios_assignment_one/Controllers/FoodViewController.swift
@@ -7,25 +7,59 @@
 
 import UIKit
 
-class FoodViewController: UIViewController {
+class FoodViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var tblFoodView: UITableView!
+    
+    let healthyFoods = ["Apple", "Orange", "Pear", "Grapefruit", "Potato", "Tomato", "Leek", "Tangerine"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tblFoodView.delegate = self;
+        tblFoodView.dataSource = self;
+        
         // Do any additional setup after loading the view.
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
     }
-    */
-
+    
+    
+      func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+          print("You taped me");
+      }
+      
+      
+      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return healthyFoods.count;
+      }
+      
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = healthyFoods[indexPath.row];
+        return cell;
+      }
+  
+    
 }
+
+//extension ViewController:UITableViewDelegate{
+//    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print("You taped me");
+//    }
+//}
+//extension ViewController:UITableViewDataSource{
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 3;
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = "Hellow World";
+//        return cell;
+//    }
+//}
---
+++ ios_assignment_one/Model/Models.swift
@@ -0,0 +1,18 @@
+//
+//  Models.swift
+//  ios_assignment_one
+//
+//  Created by Macbook on 2/28/21.
+//
+
+
+import UIKit
+import Foundation
+
+struct FoodItem {
+    var foodImage: UIImage
+    var foodName: String
+    var foodDescription: String
+    var foodPrice: Float
+    var distance: Int
+}
